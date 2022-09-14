a8eb6f089d6e58eae71b51db99ae4b8e6ebae636
DINKONDBD <70340226+DINKONDBD@users.noreply.github.com>
2022-09-14 20:32:32 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+const cnslintrvl = setInterval(() =>{
+
+    document.getElementById("contheadp").innerHTML =">";
+    setTimeout(()=>{
+        document.getElementById("contheadp").innerHTML ="";
+    }, 600)
+
+},900)
